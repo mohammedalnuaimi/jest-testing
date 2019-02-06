1badba12abc04e995b52b0ed632559a85a6a0156
Mohammed Al-Nuaimi <mohammed.al-nuaimi@dazn.com>
2019-02-06 17:28:02 +0000

    adding assertinon

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@ test('should return success on a valid call', (done) => {
      axios.get("https://api.github.com/users/octocat"
     ).then(res => {
         console.log(JSON.stringify(res.data,null, 1));
+        expect(res.data.login).toBe('octocat')
         done()
     })
-}); 
+});     
